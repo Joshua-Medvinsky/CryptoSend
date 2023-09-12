648ab44d810aaa2332d917180e7d01e9713f5a1d
Joshua-Medvinsky <joshuamedvinsky33@gmail.com>
2023-09-12 00:03:07 -0700

    Deployed transaction contract to polygon and stored contract address

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -3,4 +3,11 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks:{
+    polygon:{
+      url: 'https://polygon-mainnet.g.alchemy.com/v2/OqvF8CWti3Nsx0j06fKkJU_KGWlCwH2t',
+
+      
+    }
+  }
 };
