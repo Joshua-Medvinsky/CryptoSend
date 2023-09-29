974a59d58773d44b825f1fa310168c0b03e08ae5
Joshua-Medvinsky <joshuamedvinsky33@gmail.com>
2023-09-28 18:29:03 -0700

    Got transactions fully working, improved ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,68 @@
-"use client";
-import React, { useContext } from 'react';
+"use client"
+import React, { useContext, useState } from 'react';
 import { TransactionContext } from '@/context/TransactionContext';
-import Loader from "./loader";
+import Loader from './loader';
 
-const Input = ({ placeholder, name, type, value, handleChange }: any) => (
+const Input = ({ placeholder, name, type, value, handleChange, disabled }: any) => (
   <input
     placeholder={placeholder}
     type={type}
     step="0.0001"
     value={value}
     onChange={(e) => handleChange(e, name)}
-    className="my-2 w-full rounded-sm p-2 outline-none bg-gray-100 text-gray-800 border border-gray-300 text-sm placeholder-gray-400 focus:ring focus:ring-blue-300"
+    className={`my-2 w-full rounded-sm p-2 outline-none bg-gray-100 text-gray-800 border border-gray-300 text-sm placeholder-gray-400 focus:ring focus:ring-blue-300 ${disabled ? 'bg-gray-200 cursor-not-allowed' : ''}`}
+    disabled={disabled}
   />
 );
 
 export default function Home() {
   const darkBackground = {
-    background: 'linear-gradient(to top, rgb(229, 231, 235), rgb(156, 163, 175), rgb(75, 85, 99))',
+    background:
+      'linear-gradient(to top, rgb(229, 231, 235), rgb(156, 163, 175), rgb(75, 85, 99))',
   };
-  const { connectWallet, currentAccount, formData, handleChange, sendTransaction, isLoading } = useContext(TransactionContext);
+  const {
+    connectWallet,
+    currentAccount,
+    formData,
+    handleChange,
+    sendTransaction,
+    isLoading,
+  } = useContext(TransactionContext);
 
-  const handleSubmit = (e:any) => {
-    // Add your submit logic here
-    const {addressTo,amount,keyword,message} = formData;
+  const [showConnectWalletError, setShowConnectWalletError] = useState(false);
+
+  const handleSubmit = (e: any) => {
     e.preventDefault();
 
-    if(!addressTo||!amount||!keyword||!message) return;
+    const { addressTo, amount, keyword, message } = formData;
+
+    if (!currentAccount) {
+      setShowConnectWalletError(true);
+      return;
+    }
+
+    if (!addressTo || !amount || !keyword || !message) return;
 
     sendTransaction();
   };
 
+  const handleConnectWalletClick = () => {
+    setShowConnectWalletError(false);
+    connectWallet();
+  };
+
   return (
-    <div style={darkBackground} className="min-h-screen text-gray-800">
-      <header className="p-4 flex justify-between items-center">
+    <div style={darkBackground} className="min-h-screen text-gray-800 font-sans">
+      <header className="p-4 flex justify-between items-center bg-white shadow-md">
         <div className="flex items-center">
-          <div className="rounded-lg border-2 border-white h-12 w-12 flex items-center justify-center overflow-hidden">
-            <img src="/logo.jpg" alt="Logo" className="h-12 w-12" />
-          </div>
-          <h1 className="text-3xl font-bold ml-2 text-gray-800">Cryptosend</h1>
+        <div className="rounded-full h-12 w-12 flex items-center justify-center overflow-hidden m-1 custom-border">
+          <img src="/logo.jpg" alt="Logo" className="h-12 w-12 rounded-full" />
+        </div>
+
+          <h1 className="text-4xl font-bold ml-4 text-gray-800">Cryptosend</h1>
         </div>
         <nav className="text-lg">
-          <ul className="flex space-x-4">
+          <ul className="flex space-x-6">
             <li className="cursor-pointer hover:underline">Market</li>
             <li className="cursor-pointer hover:underline">Exchange</li>
             <li className="cursor-pointer hover:underline">Wallets</li>
@@ -48,63 +70,80 @@ export default function Home() {
         </nav>
       </header>
 
-      <main className="flex flex-col items-center justify-center h-screen">
-        <section className="bg-white p-6 rounded-lg shadow-lg flex flex-col items-center mt-4">
-          <h2 className="text-xl font-bold mb-4 text-gray-800">Send Crypto</h2>
-          <p className="text-gray-600 mb-4">
-            Cryptosend allows you to securely send and receive cryptocurrencies. Start your crypto journey with us!
+      <main className="flex flex-col items-center justify-center min-h-screen">
+        <section className="bg-white p-8 rounded-lg shadow-lg flex flex-col items-center mt-8">
+          <h2 className="text-3xl font-bold mb-6 text-gray-800">Send Crypto Across the World</h2>
+          <p className="text-gray-600 mb-6">
+            Explore the crypto world with ease. Buy cryptocurrencies easily with CryptoSend.
           </p>
-
           {!currentAccount && (
-            <button className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600" onClick={connectWallet}>
+            <button
+              type="button"
+              onClick={handleConnectWalletClick}
+              className="bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-600 transition duration-300 ease-in-out"
+            >
               Connect Wallet
             </button>
           )}
 
-          <div style={darkBackground} className="p-5 sm:w-96 w-full flex flex-col items-center rounded-lg mt-4">
+          <div style={darkBackground} className="p-6 sm:w-96 w-full flex flex-col items-center rounded-lg mt-8">
             <Input
               placeholder="Address To"
               name="addressTo"
               type="text"
+              value={formData.addressTo}
               handleChange={handleChange}
+              disabled={!currentAccount}
             />
             <Input
               placeholder="Amount (ETH)"
               name="amount"
               type="number"
+              value={formData.amount}
               handleChange={handleChange}
+              disabled={!currentAccount}
             />
             <Input
               placeholder="Keyword (Gif)"
               name="keyword"
               type="text"
+              value={formData.keyword}
               handleChange={handleChange}
+              disabled={!currentAccount}
             />
             <Input
               placeholder="Enter Message"
               name="message"
               type="text"
+              value={formData.message}
               handleChange={handleChange}
+              disabled={!currentAccount}
             />
 
-            <div className="h-[1px] w-full bg-gray-400 my-2" />
+            <div className="h-[1px] w-full bg-gray-400 my-4" />
 
             {isLoading ? (
               <Loader />
             ) : (
-              <button
-                type="button"
-                onClick={handleSubmit}
-                className="text-white w-full mt-2 bg-blue-500 p-2 rounded-full cursor-pointer hover:bg-blue-600 focus:ring focus:ring-blue-300"
-              >
-                Send now
-              </button>
+              <>
+                {showConnectWalletError && (
+                  <p className="text-red-600 text-sm mt-2">Connect your wallet first</p>
+                )}
+                <button
+                  type="button"
+                  onClick={handleSubmit}
+                  className={`text-white w-full py-2 px-4 bg-blue-500 rounded-full hover:bg-blue-600 focus:ring focus:ring-blue-300 ${(!currentAccount || showConnectWalletError) ? 'bg-gray-300 cursor-not-allowed' : ''}`}
+                  disabled={!currentAccount || showConnectWalletError}
+                >
+                  Send Now
+                </button>
+              </>
             )}
           </div>
         </section>
       </main>
 
-      <footer className="text-center p-4">
+      <footer className="text-center p-4 bg-white shadow-md">
         &copy; 2023 Cryptosend. All rights reserved.
       </footer>
     </div>
--- context/TransactionContext.jsx
+++ context/TransactionContext.jsx
@@ -23,7 +23,9 @@ export const TransactionContext = React.createContext();
     export const TransactionProvider = ({children}) =>{
         const [currentAccount,setCurrentAccount] = useState('');
         const[formData, setFormData] = useState({addressTo:'',amount:'',keyword:'',message:''});
-
+        const[isLoading, setIsLoading] = useState(false);
+        const[transactionCount,setTransactionCount] =useState(0);
+       // localStorage.getItem('transactionCount')
         const handleChange= (e, name) =>{
             setFormData((prevState)=>({...prevState,[name]:e.target.value}));
         }
@@ -82,8 +84,18 @@ export const TransactionContext = React.createContext();
                     }]
                 })
 
-                const transactionHash = await transactionContract.addToBlockchain(addressTo,parsedAmount,message,keyword);
+                const transactionHash = await transactionContract.addToBlockChain(addressTo,parsedAmount,message,keyword);
 
+                setIsLoading(true);
+                console.log('Loading - ${transactionHash.hash}');
+                await transactionHash.wait()
+
+                setIsLoading(false);
+                console.log('Success - ${transactionHash.hash}');
+
+                const transactionCount = await transactionContract.getTransactionCount();
+
+                setTransactionCount(transactionCount.toNumber());
             }catch(error){
                 console.log(error);
                 throw new Error("No Ethereum Object")
