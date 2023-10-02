e21a46cdf66e9e083c02668661666dcc18bff5a2
Joshua-Medvinsky <joshuamedvinsky33@gmail.com>
2023-10-01 22:38:32 -0700

    improved look of first page

--- app/components/services.jsx
+++ app/components/services.jsx
@@ -18,8 +18,9 @@ const ServiceCard = ({ color, title, icon, subtitle }) => (
 );
 
 const Services = () => (
-  <div className="flex w-full justify-center items-center gradient-bg-services">
+    <div className="flex w-full justify-center items-center gradient-bg-services">
     <div className="flex flex-row-reverse md:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
+
       <div className="flex-1 flex flex-col justify-start items-start">
         <h1 className="text-black text-3xl sm:text-5xl py-2 text-gradient ">
           Services We Keep Enhancing
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import React, { useContext, useState } from 'react';
 import { TransactionContext } from '@/context/TransactionContext';
 import Loader from './loader';
-import Services from './components/services'
+import Services from './components/services';
 import Navbar from './components/navbar';
 
 const Input = ({ placeholder, name, type, value, handleChange, disabled }: any) => (
@@ -51,90 +51,88 @@ export default function Home() {
 
   return (
     <div className="min-h-screen text-gray-800 font-sans bg-gradient-to-r from-blue-200 to-cyan-200">
-
-      <Navbar/>
+      <Navbar />
       <main className="flex flex-col items-center justify-center min-h-screen">
-        <section className="bg-white p-8 rounded-lg shadow-lg flex flex-col items-center mt-8">
-          <h2 className="text-3xl font-bold mb-6 text-gray-800">Send Crypto Across the World</h2>
-          <p className="text-gray-600 mb-6">
-            Explore the crypto world with ease. Buy cryptocurrencies easily with CryptoSend.
-          </p>
-          {!currentAccount && (
-            <button
-              type="button"
-              onClick={handleConnectWalletClick}
-              className="bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-600 transition duration-300 ease-in-out"
-            >
-              Connect Wallet
-            </button>
-          )}
-
-          <div className="bg-gradient-to-r from-blue-200 to-cyan-200 p-6 sm:w-96 w-full flex flex-col items-center rounded-lg mt-8">
-            <Input
-              placeholder="Address To"
-              name="addressTo"
-              type="text"
-              value={formData.addressTo}
-              handleChange={handleChange}
-              disabled={!currentAccount}
-            />
-            <Input
-              placeholder="Amount (ETH)"
-              name="amount"
-              type="number"
-              value={formData.amount}
-              handleChange={handleChange}
-              disabled={!currentAccount}
-            />
-            <Input
-              placeholder="Keyword (Gif)"
-              name="keyword"
-              type="text"
-              value={formData.keyword}
-              handleChange={handleChange}
-              disabled={!currentAccount}
-            />
-            <Input
-              placeholder="Enter Message"
-              name="message"
-              type="text"
-              value={formData.message}
-              handleChange={handleChange}
-              disabled={!currentAccount}
-            />
-
-            <div className="h-[1px] w-full bg-gray-400 my-4" />
-
-            {isLoading ? (
-              <Loader />
-            ) : (
-              <>
-                {showConnectWalletError && (
-                  <p className="text-red-600 text-sm mt-2">Connect your wallet first</p>
-                )}
-                <button
-                  type="button"
-                  onClick={handleSubmit}
-                  className={`text-white w-full py-2 px-4 bg-blue-500 rounded-full hover:bg-blue-600 focus:ring focus:ring-blue-300 ${(!currentAccount || showConnectWalletError) ? 'bg-gray-300 cursor-not-allowed' : ''}`}
-                  disabled={!currentAccount || showConnectWalletError}
-                >
-                  Send Now
-                </button>
-              </>
+        <section className="flex flex-col sm:flex-row items-start sm:items-center mt-8">
+          <div className="sm:w-1/2 w-full p-8">
+            <h2 className="text-3xl font-bold mb-6 text-gray-800">Send Crypto Across the World</h2>
+            <p className="text-gray-600 mb-6">
+              Explore the crypto world with ease. Buy cryptocurrencies easily with CryptoSend.
+            </p>
+            {!currentAccount && (
+              <button
+                type="button"
+                onClick={handleConnectWalletClick}
+                className="bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-600 transition duration-300 ease-in-out"
+              >
+                Connect Wallet
+              </button>
             )}
+          </div>
+          <div className="bg-white p-6 sm:w-1/2 w-full rounded-lg mt-8 shadow-lg" style={{ marginRight: '20px' }}>
+            <div className="bg-gradient-to-r from-blue-200 to-cyan-200 p-6 rounded-lg">
+              <Input
+                placeholder="Address To"
+                name="addressTo"
+                type="text"
+                value={formData.addressTo}
+                handleChange={handleChange}
+                disabled={!currentAccount}
+              />
+              <Input
+                placeholder="Amount (ETH)"
+                name="amount"
+                type="number"
+                value={formData.amount}
+                handleChange={handleChange}
+                disabled={!currentAccount}
+              />
+              <Input
+                placeholder="Keyword (Gif)"
+                name="keyword"
+                type="text"
+                value={formData.keyword}
+                handleChange={handleChange}
+                disabled={!currentAccount}
+              />
+              <Input
+                placeholder="Enter Message"
+                name="message"
+                type="text"
+                value={formData.message}
+                handleChange={handleChange}
+                disabled={!currentAccount}
+              />
+
+              <div className="h-[1px] w-full bg-gray-400 my-4" />
+
+              {isLoading ? (
+                <Loader />
+              ) : (
+                <>
+                  {showConnectWalletError && (
+                    <p className="text-red-600 text-sm mt-2">Connect your wallet first</p>
+                  )}
+                  <button
+                    type="button"
+                    onClick={handleSubmit}
+                    className={`text-white w-full py-2 px-4 bg-blue-500 rounded-full hover:bg-blue-600 focus:ring focus:ring-blue-300 ${(!currentAccount || showConnectWalletError) ? 'bg-gray-300 cursor-not-allowed' : ''}`}
+                    disabled={!currentAccount || showConnectWalletError}
+                  >
+                    Send Now
+                  </button>
+                </>
+              )}
+            </div>
           </div>
         </section>
       </main>
 
+      <Services />
 
-           <Services/>
-
-
-      <footer className="text-center p-4  shadow-md text-black">
+      <footer className="text-center p-4 shadow-md text-black">
         &copy; 2023 Cryptosend. All rights reserved.
       </footer>
     </div>
-
-    
   );
 }
